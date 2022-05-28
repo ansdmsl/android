85ba25162085e3a703f937413196ce739ae971da
ansdmsl <ansdmsl@naver.com>
2022-05-28 23:28:29 +0900

    no message

---
+++ src/main/java/com/bitc/team2/Kong1Application.java
@@ -0,0 +1,13 @@
+package com.bitc.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kong1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kong1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitc/team2/Kong1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitc.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kong1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
